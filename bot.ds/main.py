6e57473531cc6304a09ddca08b6fc7834756b734
Dev15114 <nontawatdamchawai174@gmail.com>
2024-10-30 02:55:20 +0700

    Add files via upload

---
+++ bot.ds/main.py
@@ -0,0 +1,15 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from myserver import server_on
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))
